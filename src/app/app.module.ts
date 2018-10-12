d1b67cdefdd04a7d79567bba38bc02e5404daefe
Mário Lúcio Fonseca Vilela <mariolf.vilela@gmail.com>
2018-10-12 20:00:21 -0300

    The directives , pipes and models folders contains the directives, pipes and models used across the application.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { LoaderComponent } from './shared/components/loader/loader.component';
+import { FavoriteButtonComponent } from './shared/components/buttons/favorite-button/favorite-button.component';
+import { AuthDirective } from './shared/directives/auth.directive';
+import { CapitalizePipe } from './shared/pipes/capitalize.pipe';
+import { SafePipe } from './shared/pipes/safe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoaderComponent,
+    FavoriteButtonComponent,
+    AuthDirective,
+    CapitalizePipe,
+    SafePipe
   ],
   imports: [
     BrowserModule,
@@ -15,6 +25,7 @@ import { CoreModule } from './core/core.module';
     CoreModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [LoaderComponent, FavoriteButtonComponent]
 })
 export class AppModule { }
